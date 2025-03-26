86ff2e469c89813c940871c277769b5b94b7719b
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-26 20:00:55 +0300

    Initial Commit

---
+++ Optimizasyon/OptimizasyonApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptimizasyonApp.swift
+//  Optimizasyon
+//
+//  Created by mehmet Çelik on 26.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct OptimizasyonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
